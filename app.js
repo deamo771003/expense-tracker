27922957173df08a64811e4cade244debdfbdc8e
JimmyLin <disillusioned2077@gmail.com>
2023-05-07 00:02:08 +0800

    feat: add register feature and connect flash

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ require('./config/mongoose')
 const session = require('express-session')
 // const bcrypt = require('bcryptjs')
 const usePassport = require('./config/passport')
+const flash = require('connect-flash')
 
 // hbs
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -28,16 +29,24 @@ app.use(session({
 // passport
 usePassport(app)
 
+// connect-flash
+app.use(flash())
+
 // body-parser
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // method-override
 app.use(methodOverride('_method'))
 
+// 導入public內的.css
+app.use(express.static('public'))
+
 // run routes前檢查驗證 代表這組 middleware 會作用於所有的路由
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated() // 把 req.isAuthenticated() 回傳的布林值，交接給 res 使用
   res.locals.user = req.user // 把user給res使用
+  res.locals.success_msg = req.flash('success_msg') // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg') // 設定 warning_msg 訊息
   next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
+const User = require('../../models/user')
 
 // login
 router.get('/login', (req, res) => {
@@ -15,6 +17,38 @@ router.post('/login', passport.authenticate('local', {
 // register
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: 'email、password、confirmPassword 必填' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: 'password 與 confirmPassword 不符' })
+  }
+  if (errors.length) {
+    return res.render('register', { errors, name, email, password, confirmPassword })
+  }
+
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        errors.push({ message: '這個 email 已經註冊過了。' })
+        return res.render('register', { errors, name, email, password, confirmPassword })
+      }
+    })
+  return bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(password, salt))
+    .then(hash => User.create({
+      name,
+      email,
+      password: hash
+    }))
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
 })
 
 // logout
