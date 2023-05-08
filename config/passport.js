352024ee00f8900669205bb1293da79e44057cd9
JimmyLin <disillusioned2077@gmail.com>
2023-05-08 16:16:57 +0800

    feat: add fackbook login feature, modify new page UI

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
-const flash = require('connect-flash')
+const FacebookStrategy = require('passport-facebook').Strategy
 
 module.exports = app => {
   // 初始化passport
@@ -26,6 +26,34 @@ module.exports = app => {
           })
       })
       .catch(err => done(err, false))
+  }))
+
+  // facebook登入策略
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID, // 填入FB專案編號
+    clientSecret: process.env.FACEBOOK_SECRET, // 填入FB專案密鑰
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName'] // 需與FB拿回的資料,displayName = fb上公開名稱
+  }, (accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+    User.findOne({ email })
+      .then(user => {
+        if (user) return done(null, user) // 如果database已有資料就回傳
+        // 由於屬性 password 有設定必填，我們還是需要幫使用 Facebook 註冊的使用者製作密碼。因此這裡刻意設定一串亂碼
+        const randomPassword = Math.random().toString(36).slice(-8) // 亂數a-z 1-9字串，取最後8位字串
+        // 加鹽雜湊加密
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt)) // randomPassword加鹽後雜湊
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
+          // passport驗證成功後，呼叫done(err,user,info)，本案為驗證成功done(沒錯誤,user傳給done)
+          .then(user => done(null, user))
+          .catch(err => done(err, false))
+      })
   }))
 
   // 序列化 database 找物件 並把id存入session
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const home = require('./modules/home')
 const users = require('./modules/users')
 const records = require('./modules/records')
 const { authenticator } = require('../middleware/auth')
+const auth = require('./modules/auth')
 
-router.use('/users', users)
 router.use('/records', authenticator, records)
+router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
